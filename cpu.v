4bfa12101224957a4605db5f78485c6863ee7318
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2025-04-27 15:33:15 +0900

    tmp

--- cpu.v
+++ cpu.v
@@ -4,6 +4,14 @@ module m_adder(w_in1, w_in2, w_out);
     assign w_out = w_in1 + w_in2;
 endmodule
 
+module m_alu(w_in1, w_in2, w_out, w_tkn);
+    input wire [31:0] w_in1, w_in2;
+    output wire [31:0] w_out;
+    output wire w_tkn;
+    assign w_out = w_in1 + w_in2;
+    assign w_tkn = w_in1 != w_in2;
+endmodule
+
 module m_mux(w_in1, w_in2, w_sel, w_out);
     input wire [31:0] w_in1, w_in2;
     input wire w_sel;
@@ -74,31 +82,37 @@ module m_am_dmem(w_clk, w_adr, w_we, w_wd, w_rd);
     integer i; initial for (i=0; i<64; i=i+1) mem[i] = 32'd0;
 endmodule
 
-module m_proc4(w_clk);
+module m_proc5(w_clk);
     input wire w_clk;
     wire [31:0] w_npc, w_ir, w_imm, w_r1, w_r2, w_s2, w_rt;
-    wire [31:0] w_alu, w_ldd;
+    wire [31:0] w_alu, w_ldd, w_tpc, w_pcin;
     reg [31:0] r_pc = 0;
-    wire w_r, w_i, w_s, w_b, w_u, w_j, w_ld;
+    wire w_r, w_i, w_s, w_b, w_u, w_j, w_ld, w_tkn;
+    m_mux m11(w_npc, w_npc, w_b & w_tkn, w_pcin);
     m_adder m2 (32'h4, r_pc, w_npc);
     m_am_imem m3 (r_pc, w_ir);
     m_gen_imm m4 (w_ir, w_imm, w_r, w_i, w_s, w_b, w_u, w_j, w_ld);
-    m_RF m5 (w_clk, w_ir[19:15], w_ir[24:20], w_r1, w_r2, w_ir[11:7], !w_s, w_rt);
-    m_mux m6 (w_r2, w_imm, !w_r, w_s2);
-    m_adder m7 (w_r1, w_s2, w_alu);
-    m_am_dmem m8 (w_clk, w_alu, w_s, w_r2, w_ldd);
-    m_mux m9 (w_alu, w_ldd, w_ld, w_rt);
+    m_RF m5 (w_clk, w_ir[19:15], w_ir[24:20], w_r1, w_r2, w_ir[11:7], !w_s & !w_b, w_rt);
+    m_adder m6 (w_imm, r_pc, w_tpc);
+    m_mux m7 (w_r2, w_imm, !w_r, w_s2);
+    m_alu m8 (w_r1, w_s2, w_alu, w_tkn);
+    m_am_dmem m9 (w_clk, w_alu, w_s, w_r2, w_ldd);
+    m_mux m10 (w_alu, w_ldd, w_ld, w_rt);
     always @(posedge w_clk) r_pc <= w_npc;
+    wire w_halt = (!w_s & !w_b & w_ir[11:7] == 5'd30);
 endmodule
 
 module m_top();
     reg r_clk=0; initial #150 forever #50 r_clk = ~r_clk;
-    m_proc4 m (r_clk);
+    m_proc5 m (r_clk);
     initial begin
-        m.m3.mem[0]={12'd7,5'd0,3'd0,5'd1,7'h13};     // addi x1,x0,7
-        m.m3.mem[1]={7'd0,5'd1,5'd0,3'h2,5'd8,7'h23}; // sw x1, 8(x0)
-        m.m3.mem[2]={12'd8,5'd0,3'b010,5'd2,7'h3};    // lw x2, 8(x0)
+        m.m3.mem[0]={12'd5,5'd0,3'h0,5'd1,7'h13};       //  addi x1,x0,5
+        m.m3.mem[1]={7'd0,5'd1,5'd1,3'h0,5'd2,7'h33};   //  add  x2,x1,x1
+        m.m3.mem[2]={12'd1,5'd1,3'd0,5'd1,7'h13};       //L:addi x1,x1,1
+        m.m3.mem[3]={~7'd0,5'd2,5'd1,3'h1,5'h1d,7'h63}; //  bne  x1,x2,L
+        m.m3.mem[4]={12'd9,5'd1,3'd0,5'd10,7'h13};      //  addi x10,x1,9
     end
-    initial #99 forever #100 $display("%3d %d %d %d", $time, m.w_r1, m.w_s2, m.w_rt);
-    initial #400 $finish;
+    initial #99 forever #100 $display("%4d %h %h %d %d %d",
+        $time, m.r_pc, m.w_imm, m.w_r1, m.w_s2, m.w_rt);
+    initial #1400 $finish;
 endmodule
